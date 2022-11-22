f4529a172422d8bdb5b5bcd85ca193f1da9b423a
Hammam <mohammedhmmam@gmail.com>
2022-11-23 01:14:57 +0200

    Create Node Class

---
+++ linked_list/stack_using_linked_list/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//Node Class
+class Node{
+public:
+    int data;
+    Node* next;
+
+    //constructor
+    Node(){
+        data = 0;
+        next = NULL;
+    }
+
+};
+
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
